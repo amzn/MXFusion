Added broadcasting for trace and log_multivariate_gamma functions Fixed tests for above Wishart log pdf test now passing

mxfusion/components/distributions/wishart.py
@@ -156,15 +156,24 @@ class Wishart(Distribution):
         F = get_default_MXNet_mode() if F is None else F
 
         # Constants
-        dimension = scale.shape[0]
-        a = degrees_of_freedom - dimension - 1
-        b = degrees_of_freedom * dimension * np.log(2)
+        num_samples, num_data_points, dimension, _ = scale.shape
+
+        # Note that the degrees of freedom should be a float for most of the remaining calculations
+        df = degrees_of_freedom.astype(random_variable.dtype)
+        a = df - dimension - 1
+        b = df * dimension * np.log(2)
+
+        # Make copies of the constants
+        df = df.tile(num_data_points).reshape((num_samples, num_data_points))
+        a = a.tile(num_data_points).reshape((num_samples, num_data_points))
+        b = b.tile(num_data_points).reshape((num_samples, num_data_points))
 
         log_det_X = sp.log_determinant(random_variable, F)
-        log_gamma_np = sp.log_mv_gamma(degrees_of_freedom / 2, dimension)
-        tr_v_inv_x = sp.trace(sp.solve(scale, random_variable))
+        log_det_V = sp.log_determinant(scale, F)
+        log_gamma_np = sp.log_multivariate_gamma(df / 2, dimension, F)
+        tr_v_inv_x = sp.trace(sp.solve(scale, random_variable), F)
 
-        return 0.5 * (((a * log_det_X) - tr_v_inv_x - b) / 2) - log_gamma_np
+        return 0.5 * ((a * log_det_X) - tr_v_inv_x - b - (df * log_det_V)) - log_gamma_np
 
     @WishartDrawSamplesDecorator()
     def draw_samples(self, degrees_of_freedom, scale, rv_shape, num_samples=1, F=None):

mxfusion/util/special.py
@@ -20,10 +20,11 @@ def log_determinant(A, F=None):
 
 
 # noinspection PyPep8Naming
-def log_mv_gamma(x, p, F=None):
+def log_multivariate_gamma(x, p, F=None):
     """
     Compute the log of the multivariate gamma function of dimension p
     See https://en.wikipedia.org/wiki/Multivariate_gamma_function
+    This will broadcast the first two extra dimensions if necessary
 
     :param x: input variable
     :param p: dimension
@@ -32,24 +33,35 @@ def log_mv_gamma(x, p, F=None):
     """
     F = get_default_MXNet_mode() if F is None else F
 
-    if p == 1:
-        # note that \Gamma_1(x) reduces to the ordinary gamma function
-        return F.gammaln(x)
+    def log_gamma_sum(a):
+        # Sum over univariate log gamma functions
+        if p == 1:
+            # note that \Gamma_1(x) reduces to the ordinary gamma function
+            return F.gammaln(a)
+        r = F.zeros(p)
+        for k in range(1, p + 1):
+            r[k - 1] = F.gammaln(a + ((1. - k) / 2))
+        return F.sum(r)
+
+    p_mx = F.array([p], dtype=x.dtype)
 
     # leading constant
-    c = p * (p - 1) / 4 * np.log(np.pi)
+    c = p_mx * (p_mx - 1) / 4 * np.log(np.pi)
 
-    # Sum over univariate gamma functions
-    j = F.arange(0, p)
-    g = F.gammaln(F.broadcast_sub(x / 2, j / 2))
-
-    return F.sum(g) + c
+    shape = x.shape
+    x_flat = x.reshape(-1)
+    result_flat = F.zeros(x_flat.shape)
+    for i in range(len(x_flat)):
+        result_flat[i] = log_gamma_sum(x_flat[i])
+    result = result_flat.reshape(shape)
+    return result + c
 
 
 # noinspection PyPep8Naming
 def trace(A, F=None):
     """
-    Compute the trace of the matrix A
+    Compute the trace of the matrix A.
+    This will broadcast the first two extra dimensions if necessary
 
     :param A: input matrix
     :param F: the MXNet computation mode (mxnet.symbol or mxnet.ndarray).
@@ -57,9 +69,30 @@ def trace(A, F=None):
     """
     F = get_default_MXNet_mode() if F is None else F
 
-    return F.sum(F.diag(A))
+    if A.ndim > 4:
+        raise ValueError("Cannot broadcast more than two dimensions")
+
+    if A.ndim == 2:
+        F.sum(F.diag(A))
+
+    # TODO: make use of the nd-array support for diag when it's available
+    # see: https://github.com/apache/incubator-mxnet/pull/12430
+
+    result = F.zeros(A.shape[:-2])
+
+    if A.ndim == 3:
+        for i in range(A.shape[1]):
+            result[i] = F.sum(F.diag(A[i, :, :]))
+
+    if A.ndim == 4:
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                result[i, j] = F.sum(F.diag(A[i, j, :, :]))
+
+    return result
 
 
+# noinspection PyPep8Naming
 def solve(A, B, F=None):
     """
     Solve the equation AX = B: X = A^{-1}B
@@ -76,6 +109,8 @@ def solve(A, B, F=None):
     :param F: the MXNet computation mode (mxnet.symbol or mxnet.ndarray).
     :return:
     """
+    F = get_default_MXNet_mode() if F is None else F
+
     L = F.linalg.potrf(A)
     y = F.linalg.trsm(L, B)
     X = F.linalg.trsm(L, y, transpose=1)

testing/distributions/wishart_test.py
@@ -12,12 +12,13 @@ from mxfusion.util.testutils import MockMXNetRandomGenerator, numpy_array_reshap
 @pytest.mark.usefixtures("set_seed")
 class TestWishartDistribution(object):
 
-    @pytest.mark.parametrize("dtype, degrees_of_freedom, random_state, scale_is_samples, "
+    @pytest.mark.parametrize("dtype_dof, dtype, degrees_of_freedom, random_state, scale_is_samples, "
                              "rv_is_samples, num_data_points, num_samples",
                              [
-                                 (np.float32, 2, 0, True, True, 3, 5),
+                                 # (np.int32, np.float32, 1, 0, False, True, 3, 6),
+                                 (np.int32, np.float32, 2, 0, False, True, 3, 6),
                              ])
-    def test_log_pdf_with_broadcast(self, dtype, degrees_of_freedom, random_state,
+    def test_log_pdf_with_broadcast(self, dtype_dof, dtype, degrees_of_freedom, random_state,
                                     scale_is_samples, rv_is_samples, num_data_points, num_samples):
         # Create random variables
         rv = np.zeros((num_samples, num_data_points, degrees_of_freedom, degrees_of_freedom))
@@ -30,8 +31,8 @@ class TestWishartDistribution(object):
         # TODO: should be PD not PSD?
         scale = make_spd_matrix(n_dim=degrees_of_freedom, random_state=random_state)
 
-        degrees_of_freedom_mx = mx.nd.array([degrees_of_freedom], dtype=int)
-        # degrees_of_freedom = degrees_of_freedom_mx.asnumpy()
+        degrees_of_freedom_mx = mx.nd.array([degrees_of_freedom], dtype=dtype_dof)
+        degrees_of_freedom = degrees_of_freedom_mx.asnumpy()[0]
 
         scale_mx = mx.nd.array(scale, dtype=dtype)
         if not scale_is_samples:
@@ -46,12 +47,12 @@ class TestWishartDistribution(object):
         is_samples_any = scale_is_samples or rv_is_samples
         rv_shape = rv.shape[1:]
 
-        degrees_of_freedom = 1 + len(rv.shape) if is_samples_any and not rv_is_samples else len(rv.shape)
+        # degrees_of_freedom = 1 + len(rv.shape) if is_samples_any and not rv_is_samples else len(rv.shape)
         scale_np = np.broadcast_to(scale, rv.shape)
         rv_np = numpy_array_reshape(rv, is_samples_any, degrees_of_freedom)
 
-        rand = np.random.rand(num_samples, *rv_shape)
-        rand_gen = MockMXNetRandomGenerator(mx.nd.array(rand.flatten(), dtype=dtype))
+        # rand = np.random.rand(num_samples, *rv_shape)
+        # rand_gen = MockMXNetRandomGenerator(mx.nd.array(rand.flatten(), dtype=dtype))
 
         r = []
         for s in range(num_samples):
@@ -61,7 +62,7 @@ class TestWishartDistribution(object):
             r.append(a)
         log_pdf_np = np.array(r)
 
-        var = Wishart.define_variable(shape=rv_shape, dtype=dtype, rand_gen=rand_gen).factor
+        var = Wishart.define_variable(shape=rv_shape, dtype=dtype, rand_gen=None).factor
         variables = {var.degrees_of_freedom.uuid: degrees_of_freedom_mx, var.scale.uuid: scale_mx,
                      var.random_variable.uuid: rv_mx}
         log_pdf_rt = var.log_pdf(F=mx.nd, variables=variables)

testing/util/special_test.py
@@ -1,7 +1,7 @@
 import pytest
 import mxnet as mx
 import numpy as np
-from mxfusion.util.special import log_determinant, log_mv_gamma
+from mxfusion.util.special import log_determinant, log_multivariate_gamma
 from sklearn.datasets import make_spd_matrix
 from itertools import product
 from scipy.special import multigammaln
@@ -31,13 +31,24 @@ class TestSpecialFunctions:
 
         assert abs(mx_logdet - np_logdet) < 1e-5 * n_dim
 
-    @pytest.mark.parametrize("n_dim, random_state", list(product((10, 100, 1000), range(1, 4))))
-    def test_log_mv_gamma(self, n_dim, random_state):
+    @pytest.mark.parametrize("n_data_points, n_dim, random_state", list(product((4, ), (1, 10, 100), range(1, 4))))
+    def test_log_mv_gamma(self, n_data_points, n_dim, random_state):
         np.random.seed(random_state)
-        x = np.random.rand() + n_dim
-        a = mx.nd.array([x])
-        b = log_mv_gamma(a, n_dim, mx.nd)
+        x = np.random.rand(n_data_points) + n_dim
+        a = mx.nd.array(x)
+        b = log_multivariate_gamma(a, n_dim, mx.nd)
 
-        mx_val = b.asnumpy()[0]
-        sp_val = multigammaln(x / 2, n_dim)
-        assert abs(mx_val - sp_val) < 1e-3 * n_dim
+        mx_val = b.asnumpy()
+        sp_val = multigammaln(x, n_dim)
+        assert np.allclose(mx_val, sp_val, atol=1e-3 * n_dim)
+
+    @pytest.mark.parametrize("n_samples, n_data_points, n_dim, random_state", [(5, 4, 10, 0)])
+    def test_log_mv_gamma_broadcast(self, n_samples, n_data_points, n_dim, random_state):
+        np.random.seed(random_state)
+        x = np.random.rand(n_samples, n_data_points) + n_dim
+        a = mx.nd.array(x)
+        b = log_multivariate_gamma(a, n_dim, mx.nd)
+
+        mx_val = b.asnumpy()
+        sp_val = multigammaln(x, n_dim)
+        assert np.allclose(mx_val, sp_val, atol=1e-3 * n_dim)