f9dc2acce452a0d7bdc415a079520239ca8977a2
Tom Diethe <tdiethe@amazon.com>
2018-10-17 23:10:08 +0100

    Added NormalMeanPrecision and MultivariateNormalMeanPrecision - versions of the Normal and MultivariateNormal that take mean and precision as parameters instead of mean and variance Other minor fixes.

--- mxfusion/components/distributions/__init__.py
+++ mxfusion/components/distributions/__init__.py
@@ -15,11 +15,11 @@ Submodules
     gp
 """
 
-__all__ = ['categorical', 'distribution', 'normal', 'pointmass', 'rand_gen',
-           'univariate','gp', 'wishart', 'beta']
+__all__ = ['categorical', 'distribution', 'normal', 'pointmass', 'random_gen',
+           'univariate', 'gp', 'wishart', 'beta']
 
 from .distribution import Distribution
-from .normal import Normal, MultivariateNormal
+from .normal import Normal, MultivariateNormal, NormalMeanPrecision, MultivariateNormalMeanPrecision
 from .pointmass import PointMass
 from .categorical import Categorical
 from .gp import GaussianProcess, ConditionalGaussianProcess
