72e07ce10046f03652f938f316ae15c33eadee75
Zhenwen Dai <zhenwendai@users.noreply.github.com>
2018-10-17 17:37:11 +0100

    Implement Module, GPRegression, SparseGPRegression and SVGPRegression… (#42)
    * Changed the compute interface of InferenceAlgorithm by combining the three arguments (data, parameters and constants) into a single argument variables.
    
    * implemented GPRegression module. The SparseGPRegression module is under development.
    
    * implemented SparseGPRegression Module
    
    * Implement SVGPRegression module.
    
    * Implemented the prediction method for SVGPRegression.
    
    * Implement the prediction of sparseGP in a temporal way. 
    Implement set_parameter in InferenceAlgorithm.
    
    * Fix sparse gp and svgp inducing point dimension issue.
    
    * return sparse gp prediction mean and variance.
    
    * remove print
    
    * add last iteration
    
    * allow jitter change in sparsegp
    
    * allow jitter change in sparsegp
    
    * the unfinished implementation of svgp module.
    
    * refactor functions with the new MXFusionFunction class
    
    * add mxfusion_function.py
    
    * Change Kernel as MXFusionFunction.
    
    * bug fix and add the kernel test case for MXFusionFunction
    
    * add the test case of adding an "add" kernel into an "add" kernel.
    
    * Add the test cases for MXFusionFunction.
    
    * Add docstring.
    
    * Introduce the MXFusionFunction base class and implement GP kernels as MXFusionFunction.
    
    * keep the testcase align with the existing interface
    
    * Bump the version.
    
    * Implement the clone function for GP kernels and GP distributions.
    
    * progress on gp sampling algorithm
    
    * Implement GPRegression with log_pdf and sampling.
    
    * Implement a log_pdf test for GPRegression.
    
    * Add the actual test file.
    
    * Implement sparse GP regression test.
    
    * Add the jitter that matches with GPy results.
    
    * Add test cases for SVGP regression.
    
    * Implement draw_sample test for GPRegression.
    
    * Add docstring.
    
    * Implement Module, GPRegression, SparseGPRegression and SVGPRegression Module.
    
    * Implement the prediction inference for GP modules.
    
    * Changed the name of GP module algorithms and the default AddKernel name. 
    
    Refactor the prediction algorithm to support other prediction 
    algorithms.

--- mxfusion/modules/gp_modules/__init__.py
+++ mxfusion/modules/gp_modules/__init__.py
@@ -7,3 +7,9 @@ Submodules
     :toctree: _autosummary
 
 """
+
+__all__ = ['gp_regression', 'sparsegp_regression', 'svgp_regression']
+
+from .gp_regression import GPRegression
+from .sparsegp_regression import SparseGPRegression
+from .svgp_regression import SVGPRegression
---
+++ testing/modules/sparsegpregression_test.py
@@ -0,0 +1,122 @@
+import pytest
+import mxnet as mx
+import numpy as np
+from mxfusion.models import Model
+from mxfusion.modules.gp_modules import SparseGPRegression
+from mxfusion.components.distributions.gp.kernels import RBF
+from mxfusion.components import Variable
+from mxfusion.inference import Inference, MAP, ModulePredictionAlgorithm, TransferInference
+from mxfusion.components.variables.var_trans import PositiveTransformation
+
+import matplotlib
+matplotlib.use('Agg')
+import GPy
+
+
+class TestSparseGPRegressionModule(object):
+
+    def test_log_pdf(self):
+        np.random.seed(0)
+        D = 2
+        X = np.random.rand(10, 3)
+        Y = np.random.rand(10, D)
+        Z = np.random.rand(3, 3)
+        noise_var = np.random.rand(1)
+        lengthscale = np.random.rand(3)
+        variance = np.random.rand(1)
+
+        m_gpy = GPy.models.SparseGPRegression(X=X, Y=Y, Z=Z, kernel=GPy.kern.RBF(3, ARD=True, lengthscale=lengthscale, variance=variance), num_inducing=3)
+        m_gpy.likelihood.variance = noise_var
+
+        l_gpy = m_gpy.log_likelihood()
+
+        dtype = 'float64'
+        m = Model()
+        m.N = Variable()
+        m.X = Variable(shape=(m.N, 3))
+        m.Z = Variable(shape=(3, 3), initial_value=mx.nd.array(Z, dtype=dtype))
+        m.noise_var = Variable(transformation=PositiveTransformation(), initial_value=mx.nd.array(noise_var, dtype=dtype))
+        kernel = RBF(input_dim=3, ARD=True, variance=mx.nd.array(variance, dtype=dtype), lengthscale=mx.nd.array(lengthscale, dtype=dtype), dtype=dtype)
+        m.Y = SparseGPRegression.define_variable(X=m.X, kernel=kernel, noise_var=m.noise_var, inducing_inputs=m.Z, shape=(m.N, D), dtype=dtype)
+        m.Y.factor.sgp_log_pdf.jitter = 1e-8
+
+        observed = [m.X, m.Y]
+        infr = Inference(MAP(model=m, observed=observed), dtype=dtype)
+
+        loss, _ = infr.run(X=mx.nd.array(X, dtype=dtype), Y=mx.nd.array(Y, dtype=dtype))
+        l_mf = -loss
+
+        assert np.allclose(l_mf.asnumpy(), l_gpy)
+
+    def test_prediction(self):
+        np.random.seed(0)
+        X = np.random.rand(10, 3)
+        Y = np.random.rand(10, 1)
+        Z = np.random.rand(3, 3)
+        noise_var = np.random.rand(1)
+        lengthscale = np.random.rand(3)/10.
+        variance = np.random.rand(1)
+        Xt = np.random.rand(20, 3)
+
+        m_gpy = GPy.models.SparseGPRegression(X=X, Y=Y, Z=Z, kernel=GPy.kern.RBF(3, ARD=True, lengthscale=lengthscale, variance=variance), num_inducing=3)
+        m_gpy.likelihood.variance = noise_var
+
+        dtype = 'float64'
+        m = Model()
+        m.N = Variable()
+        m.X = Variable(shape=(m.N, 3))
+        m.Z = Variable(shape=(3, 3), initial_value=mx.nd.array(Z, dtype=dtype))
+        m.noise_var = Variable(transformation=PositiveTransformation(), initial_value=mx.nd.array(noise_var, dtype=dtype))
+        kernel = RBF(input_dim=3, ARD=True, variance=mx.nd.array(variance, dtype=dtype), lengthscale=mx.nd.array(lengthscale, dtype=dtype), dtype=dtype)
+        m.Y = SparseGPRegression.define_variable(X=m.X, kernel=kernel, noise_var=m.noise_var, inducing_inputs=m.Z, shape=(m.N, 1), dtype=dtype)
+        m.Y.factor.sgp_log_pdf.jitter = 1e-8
+
+        observed = [m.X, m.Y]
+        infr = Inference(MAP(model=m, observed=observed), dtype=dtype)
+
+        loss, _ = infr.run(X=mx.nd.array(X, dtype=dtype), Y=mx.nd.array(Y, dtype=dtype))
+
+        # noise_free, diagonal
+        mu_gpy, var_gpy = m_gpy.predict_noiseless(Xt)
+
+        infr2 = TransferInference(ModulePredictionAlgorithm(m, observed=[m.X], target_variables=[m.Y]), infr_params=infr.params, dtype=np.float64)
+        res = infr2.run(X=mx.nd.array(Xt, dtype=dtype))[0]
+        mu_mf, var_mf = res[0].asnumpy()[0], res[1].asnumpy()[0]
+
+        assert np.allclose(mu_gpy, mu_mf), (mu_gpy, mu_mf)
+        assert np.allclose(var_gpy[:,0], var_mf), (var_gpy[:,0], var_mf)
+
+        # noisy, diagonal
+        mu_gpy, var_gpy = m_gpy.predict(Xt)
+
+        infr2 = TransferInference(ModulePredictionAlgorithm(m, observed=[m.X], target_variables=[m.Y]), infr_params=infr.params, dtype=np.float64)
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.noise_free = False
+        res = infr2.run(X=mx.nd.array(Xt, dtype=dtype))[0]
+        mu_mf, var_mf = res[0].asnumpy()[0], res[1].asnumpy()[0]
+
+        assert np.allclose(mu_gpy, mu_mf), (mu_gpy, mu_mf)
+        assert np.allclose(var_gpy[:,0], var_mf), (var_gpy[:,0], var_mf)
+
+        # noise_free, full_cov
+        mu_gpy, var_gpy = m_gpy.predict_noiseless(Xt, full_cov=True)
+
+        infr2 = TransferInference(ModulePredictionAlgorithm(m, observed=[m.X], target_variables=[m.Y]), infr_params=infr.params, dtype=np.float64)
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.diagonal_variance = False
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.noise_free = True
+        res = infr2.run(X=mx.nd.array(Xt, dtype=dtype))[0]
+        mu_mf, var_mf = res[0].asnumpy()[0], res[1].asnumpy()[0]
+
+        assert np.allclose(mu_gpy, mu_mf), (mu_gpy, mu_mf)
+        assert np.allclose(var_gpy, var_mf), (var_gpy, var_mf)
+
+        # noisy, full_cov
+        mu_gpy, var_gpy = m_gpy.predict(Xt, full_cov=True)
+
+        infr2 = TransferInference(ModulePredictionAlgorithm(m, observed=[m.X], target_variables=[m.Y]), infr_params=infr.params, dtype=np.float64)
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.diagonal_variance = False
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.noise_free = False
+        res = infr2.run(X=mx.nd.array(Xt, dtype=dtype))[0]
+        mu_mf, var_mf = res[0].asnumpy()[0], res[1].asnumpy()[0]
+
+        assert np.allclose(mu_gpy, mu_mf), (mu_gpy, mu_mf)
+        assert np.allclose(var_gpy, var_mf), (var_gpy, var_mf)
