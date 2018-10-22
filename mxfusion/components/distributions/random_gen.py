Fixes to uniform and Laplace random number generator due to incorrect broadcasting Added a plotting helper to testutils Added extra tests for Laplace and uniform distributions

mxfusion/components/distributions/random_gen.py
@@ -46,10 +46,10 @@ class MXNetRandomGenerator(RandomGenerator):
         """
         dtype = get_default_dtype() if dtype is None else dtype
 
-        if F is mx.ndarray:
-            return func(shape=shape, dtype=dtype, ctx=ctx, out=out, **kwargs)
+        if shape is None:
+            return func(dtype=dtype, ctx=ctx, out=out, **kwargs)
         else:
-            return func(shape=shape, dtype=dtype, out=out, **kwargs)
+            return func(shape=shape, dtype=dtype, ctx=ctx, out=out, **kwargs)
 
     @staticmethod
     def sample_normal(loc=0, scale=1, shape=None, dtype=None, out=None, ctx=None, F=None):
@@ -105,7 +105,6 @@ class MXNetRandomGenerator(RandomGenerator):
         :param F: MXNet node
         :return: Array of samples
         """
-        F = get_default_MXNet_mode() if F is None else F
         return MXNetRandomGenerator._sample_univariate(
             func=F.random.gamma, alpha=alpha, beta=beta,
             shape=shape, dtype=dtype, out=out, ctx=ctx, F=F)
@@ -125,8 +124,13 @@ class MXNetRandomGenerator(RandomGenerator):
         :param F: MXNet node
         :return: Array of samples
         """
-        return MXNetRandomGenerator._sample_univariate(func=F.random.uniform, low=low, high=high,
-                                                       shape=shape, dtype=dtype, out=out, ctx=ctx, F=F)
+        F = get_default_MXNet_mode() if F is None else F
+
+        samples = MXNetRandomGenerator._sample_univariate(
+            func=F.random.uniform, shape=shape, dtype=dtype, out=out, ctx=ctx, F=F)
+        # samples = F.broadcast_add(F.broadcast_mul(samples, F.broadcast_sub(high, low)), low)
+        samples = samples * (high - low) + low
+        return samples
 
     @staticmethod
     def sample_laplace(location=0., scale=1., shape=None, dtype=None, out=None, ctx=None, F=None):
@@ -149,7 +153,17 @@ class MXNetRandomGenerator(RandomGenerator):
         # has a Laplace distribution with parameters \mu and b
 
         U = MXNetRandomGenerator.sample_uniform(low=-0.5, high=0.5, shape=shape, dtype=dtype, out=out, ctx=ctx, F=F)
-        b_sgn_U = F.broadcast_mul(scale, F.sign(U))
+
+        if isinstance(scale, F.NDArray):
+            b_sgn_U = F.broadcast_mul(scale, F.sign(U))
+        else:
+            b_sgn_U = scale * F.sign(U)
+
         ln_1_2_U = F.log(1 - 2 * F.abs(U))
 
-        return F.broadcast_minus(location,  F.broadcast_mul(b_sgn_U, ln_1_2_U))
+        if isinstance(location, F.NDArray):
+            samples = F.broadcast_minus(location, F.broadcast_mul(b_sgn_U, ln_1_2_U))
+        else:
+            samples = location - F.broadcast_mul(b_sgn_U, ln_1_2_U)
+
+        return samples

testing/distributions/laplace_test.py
@@ -3,7 +3,7 @@ import mxnet as mx
 import numpy as np
 from mxfusion.components.variables.runtime_variable import add_sample_dimension, is_sampled_array, get_num_samples
 from mxfusion.components.distributions import Laplace
-from mxfusion.util.testutils import numpy_array_reshape
+from mxfusion.util.testutils import numpy_array_reshape, plot_univariate
 from mxfusion.util.testutils import MockMXNetRandomGenerator
 from scipy.stats import laplace
 
@@ -56,6 +56,7 @@ class TestLaplaceDistribution(object):
 
     @pytest.mark.parametrize(
         "dtype, location, location_is_samples, scale, scale_is_samples, rv_shape, num_samples", [
+            # (np.float64, np.random.rand(1), False, np.random.rand(1) + 0.1, False, (3, 1), 10000),
             (np.float64, np.random.rand(5, 2), True, np.random.rand(2) + 0.1, False, (3, 2), 5),
             (np.float64, np.random.rand(2), False, np.random.rand(5, 2) + 0.1, True, (3, 2), 5),
             (np.float64, np.random.rand(2), False, np.random.rand(2) + 0.1, False, (3, 2), 5),
@@ -93,3 +94,34 @@ class TestLaplaceDistribution(object):
         else:
             rtol, atol = 1e-4, 1e-5
         assert np.allclose(rv_samples_np, rv_samples_rt.asnumpy(), rtol=rtol, atol=atol)
+
+    def test_draw_samples_non_mock(self, plot=False):
+        # Also make sure the non-mock sampler works
+        dtype = np.float32
+        num_samples = 100000
+
+        location = np.array([0.5])
+        scale = np.array([2])
+
+        rv_shape = (1,)
+
+        location_mx = add_sample_dimension(mx.nd, mx.nd.array(location, dtype=dtype))
+        scale_mx = add_sample_dimension(mx.nd, mx.nd.array(scale, dtype=dtype))
+
+        rand_gen = None
+        var = Laplace.define_variable(shape=rv_shape, rand_gen=rand_gen, dtype=dtype).factor
+        variables = {var.location.uuid: location_mx, var.scale.uuid: scale_mx}
+        rv_samples_rt = var.draw_samples(F=mx.nd, variables=variables, num_samples=num_samples)
+
+        assert is_sampled_array(mx.nd, rv_samples_rt)
+        assert get_num_samples(mx.nd, rv_samples_rt) == num_samples
+        assert rv_samples_rt.dtype == dtype
+
+        if plot:
+            plot_univariate(samples=rv_samples_rt, dist=laplace, loc=location[0], scale=scale[0])
+
+        location_est, scale_est = laplace.fit(rv_samples_rt.asnumpy().ravel())
+        location_tol = 1e-2
+        scale_tol = 1e-2
+        assert np.abs(location[0] - location_est) < location_tol
+        assert np.abs(scale[0] - scale_est) < scale_tol

testing/distributions/uniform_test.py
@@ -3,7 +3,7 @@ import mxnet as mx
 import numpy as np
 from mxfusion.components.variables.runtime_variable import add_sample_dimension, is_sampled_array, get_num_samples
 from mxfusion.components.distributions import Uniform
-from mxfusion.util.testutils import numpy_array_reshape
+from mxfusion.util.testutils import numpy_array_reshape, plot_univariate
 from mxfusion.util.testutils import MockMXNetRandomGenerator
 from scipy.stats import uniform
 
@@ -94,3 +94,34 @@ class TestUniformDistribution(object):
         else:
             rtol, atol = 1e-4, 1e-5
         assert np.allclose(rv_samples_np, rv_samples_rt.asnumpy(), rtol=rtol, atol=atol)
+
+    def test_draw_samples_non_mock(self, plot=False):
+        # Also make sure the non-mock sampler works
+        dtype = np.float32
+        num_samples = 10000
+
+        low = np.array([0.5])
+        high = np.array([2])
+
+        rv_shape = (1,)
+
+        low_mx = mx.nd.array(low, dtype=dtype)
+        high_mx = mx.nd.array(high, dtype=dtype)
+
+        rand_gen = None
+        var = Uniform.define_variable(shape=rv_shape, rand_gen=rand_gen, dtype=dtype).factor
+        variables = {var.low.uuid: low_mx, var.high.uuid: high_mx}
+        rv_samples_rt = var.draw_samples(F=mx.nd, variables=variables, num_samples=num_samples)
+
+        assert is_sampled_array(mx.nd, rv_samples_rt)
+        assert get_num_samples(mx.nd, rv_samples_rt) == num_samples
+        assert rv_samples_rt.dtype == dtype
+
+        if plot:
+            plot_univariate(samples=rv_samples_rt, dist=uniform, loc=low[0], scale=high[0] - low[0], buffer=1)
+
+        location_est, scale_est = uniform.fit(rv_samples_rt.asnumpy().ravel())
+        location_tol = 1e-2
+        scale_tol = 1e-2
+        assert np.abs(low[0] - location_est) < location_tol
+        assert np.abs(high[0] - scale_est - location_est) < scale_tol