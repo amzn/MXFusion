Added extra Wishart test. Fixed broadcasting issues in the Wishart sampling.

testing/distributions/wishart_test.py
@@ -2,11 +2,11 @@ import pytest
 import mxnet as mx
 from sklearn.datasets import make_spd_matrix
 import numpy as np
-from scipy.stats import wishart
+from scipy.stats import wishart, chi2
 
 from mxfusion.components.distributions import Wishart
 from mxfusion.components.variables.runtime_variable import add_sample_dimension, is_sampled_array, get_num_samples
-from mxfusion.util.testutils import MockMXNetRandomGenerator, numpy_array_reshape
+from mxfusion.util.testutils import MockMXNetRandomGenerator, numpy_array_reshape, plot_univariate
 
 
 def make_spd_matrices_3d(num_samples, num_dimensions, random_state):
@@ -158,13 +158,8 @@ class TestWishartDistribution(object):
         if not scale_is_samples:
             scale_mx = add_sample_dimension(mx.nd, scale_mx)
 
-        # n_dim = 1 + len(rv.shape) if isSamples_any else len(rv.shape)
         rand = np.random.rand(num_samples, *rv_shape)
         rand_gen = MockMXNetRandomGenerator(mx.nd.array(rand.flatten(), dtype=dtype))
-        # rand_exp = np.expand_dims(rand, axis=-1)
-        # lmat = np.linalg.cholesky(var)
-        # temp1 = np.matmul(lmat, rand_exp).sum(-1)
-        # rv_samples_np = mean + temp1
 
         var = Wishart.define_variable(shape=rv_shape, dtype=dtype, rand_gen=rand_gen).factor
         variables = {var.degrees_of_freedom.uuid: degrees_of_freedom_mx, var.scale.uuid: scale_mx}
@@ -174,3 +169,34 @@ class TestWishartDistribution(object):
         assert is_sampled_array(mx.nd, draw_samples_rt)
         assert get_num_samples(mx.nd, draw_samples_rt) == num_samples, (get_num_samples(mx.nd, draw_samples_rt),
                                                                         num_samples)
+
+    def test_draw_samples_1D(self, plot=True):
+        # Also make sure the non-mock sampler works by drawing 1D samples (should collapse to chi^2)
+        dtype = np.float32
+        dtype_dof = np.int32
+        num_samples = 10000
+
+        dof = 10
+        scale = np.array([[1]])
+
+        rv_shape = scale.shape
+
+        dof_mx = mx.nd.array([dof], dtype=dtype_dof)
+        scale_mx = add_sample_dimension(mx.nd, mx.nd.array(scale, dtype=dtype))
+
+        rand_gen = None
+        var = Wishart.define_variable(shape=rv_shape, rand_gen=rand_gen, dtype=dtype).factor
+        variables = {var.degrees_of_freedom.uuid: dof_mx, var.scale.uuid: scale_mx}
+        rv_samples_rt = var.draw_samples(F=mx.nd, variables=variables, num_samples=num_samples)
+
+        assert is_sampled_array(mx.nd, rv_samples_rt)
+        assert get_num_samples(mx.nd, rv_samples_rt) == num_samples
+        assert rv_samples_rt.dtype == dtype
+
+        if plot:
+            plot_univariate(samples=rv_samples_rt, dist=chi2, df=dof)
+
+        # Note that the chi-squared fitting doesn't do a great job, so we have a slack tolerance
+        dof_est, _, _ = chi2.fit(rv_samples_rt.asnumpy().ravel())
+        dof_tol = 1e-0
+        assert np.abs(dof - dof_est) < dof_tol