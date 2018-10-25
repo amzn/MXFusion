623980bf8b0b8d74ac71954c7b4e933bba9388ea
Zhenwen Dai <zhenwend@amazon.co.uk>
2018-10-25 16:56:17 +0100

    Applied the suggestion.

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-import os
 from setuptools import setup, find_packages
 import re
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-with open('requirements/requirements.txt', 'r') as req:
-    requires = req.read().split("\n")
-
 with open('mxfusion/__version__.py', 'r') as rv:
-   text = rv.read().split('=')
-   __version__ = re.search(r'\d+\.\d+\.\d+', text[1]).group()
+    text = rv.read().split('=')
+    __version__ = re.search(r'\d+\.\d+\.\d+', text[1]).group()
 
 setup(
     name='MXFusion',  # this is the name of the package as you will import it i.e import package-name
