96ad92cb964aead0b4c64c5adde3d0489c04d53e
Mark Pullin <marpulli@amazon.com>
2019-07-03 17:28:37 +0100

    Add SGP test

--- testing/modules/sparsegpregression_test.py
+++ testing/modules/sparsegpregression_test.py
@@ -338,3 +338,43 @@ class TestSparseGPRegressionModule(object):
         kernel = RBF(input_dim=3, ARD=True, variance=mx.nd.array(variance, dtype=dtype), lengthscale=mx.nd.array(lengthscale, dtype=dtype), dtype=dtype)
         m.Y = SparseGPRegression.define_variable(X=mx.nd.zeros((2, 3)), kernel=kernel, noise_var=mx.nd.ones((1,)), dtype=dtype)
         m.clone()
+
+    def test_module_clone_prediction(self):
+        D, X, Y, Z, noise_var, lengthscale, variance = self.gen_data()
+        dtype = 'float64'
+
+        # Predict from original model
+        m = self.gen_mxfusion_model(dtype, D, Z, noise_var, lengthscale, variance)
+
+        observed = [m.X, m.Y]
+        infr = Inference(MAP(model=m, observed=observed), dtype=dtype)
+
+        loss, _ = infr.run(X=mx.nd.array(X, dtype=dtype), Y=mx.nd.array(Y, dtype=dtype), max_iter=1)
+
+        infr2 = TransferInference(ModulePredictionAlgorithm(m, observed=[m.X], target_variables=[m.Y]),
+                                  infr_params=infr.params, dtype=np.float64)
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.diagonal_variance = False
+        infr2.inference_algorithm.model.Y.factor.sgp_predict.noise_free = False
+        res = infr2.run(X=mx.nd.array(X, dtype=dtype))[0]
+        mu_mf, var_mf = res[0].asnumpy()[0], res[1].asnumpy()[0]
+
+        # Clone model
+        cloned_model = m.clone()
+
+        # Predict from cloned model
+        observed = [cloned_model.X, cloned_model.Y]
+        infr = Inference(MAP(model=cloned_model, observed=observed), dtype=dtype)
+
+        loss, _ = infr.run(X=mx.nd.array(X, dtype=dtype), Y=mx.nd.array(Y, dtype=dtype), max_iter=1)
+
+        infr2_clone = TransferInference(ModulePredictionAlgorithm(cloned_model, observed=[cloned_model.X],
+                                                                  target_variables=[cloned_model.Y]),
+                                        infr_params=infr.params, dtype=np.float64)
+
+        infr2_clone.inference_algorithm.model.Y.factor.sgp_predict.diagonal_variance = False
+        infr2_clone.inference_algorithm.model.Y.factor.sgp_predict.noise_free = False
+        res = infr2_clone.run(X=mx.nd.array(X, dtype=dtype))[0]
+        mu_mf_clone, var_mf_clone = res[0].asnumpy()[0], res[1].asnumpy()[0]
+
+        assert np.allclose(mu_mf, mu_mf_clone)
+        assert np.allclose(var_mf, var_mf_clone)
